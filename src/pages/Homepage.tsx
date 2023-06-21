95db75ce0e01803f46d7f2b342d940a2b66337a6
Devashish Sood <devashishsood9@gmail.com>
2023-06-21 01:26:06 -0400

    comp name fix

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,12 @@
-import { Body, Text } from "../styles/general-styles.js";
+import { Body, RegularText } from "../styles/general-styles.js";
 
 export default function Homepage() {
   return (
     <Body>
-      <Text>
+      <RegularText>
         I'm beginning to use my work on this website as a means for distraction
         from other work I need to get to. Time to stop for a bit lol.
-      </Text>
+      </RegularText>
     </Body>
   );
 }
